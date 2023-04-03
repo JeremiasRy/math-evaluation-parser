5169956d9aecea7f8697aaf9645b9f4d9f55d01c
JeremiasRy <ryttari.jeremias@gmail.com>
2023-04-03 09:43:16 +0300

    feat: basic math function handling

--- math evaluation parser/Program.cs
+++ math evaluation parser/Program.cs
@@ -1,9 +1,11 @@
-﻿using System.Linq;
+﻿using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
-
+using System.Xml.Schema;
 
 static string Eval(string expression)
 {
+    string[] allowedFuncs = new string[] { "log", "ln", "exp", "sqrt", "abs", "atan", "acos", "asin", "sinh", "cosh", "tanh", "tan", "sin", "cos" };
     Regex oper = new(@"\+|\*|\/|&");
     Regex mathFuncs = new(@"([a-z]+[a-z]+[a-z]?[a-z]?)");
     Regex minusOper = new(@"-{2,}");
@@ -12,10 +14,10 @@ static string Eval(string expression)
     MatchCollection functions = mathFuncs.Matches(expression);
     foreach (Match match in functions.Cast<Match>())
     {
-        if (match.Value.Contains(' '))
+        if (!allowedFuncs.Any(allowed => allowed == match.Value))
         {
             return "Error";
-        };
+        }
     }
     expression = WhiteSpaceRemover(expression);
     MatchCollection minusOperators = minusOper.Matches(expression);
@@ -36,6 +38,10 @@ static string Eval(string expression)
         return "Error";
     }
 
+    if (expression.Where(ch => ch == '(').Count() != expression.Where(ch => ch == ')').Count())
+    {
+        return "Error";
+    }
     //Evaluate brackets
     while (expression.IndexOf('(') != -1)
     {
@@ -44,21 +50,61 @@ static string Eval(string expression)
         string result = EvaluateBrackets(subString[1..^1]);
         expression = expression.Replace(subString, result);
     }
-
-    var mathOperations = oper.Matches(expression);
     string[] mathValues = oper.Split(expression);
-    if (expression.StartsWith(functions.First().Value))
-    {
-        return Math.Abs(int.Parse(expression.Replace(functions.First().Value, ""))).ToString();
-    }
 
     return "= 0";
 }
+static string EvaluateFunction(string function, string value)
+{   
+    switch (function)
+    {
+        case "sin":
+            {
+                return Math.Sin(double.Parse(value)).ToString();
+            } 
+        case "cos":
+            {
+                return Math.Cos(double.Parse(value)).ToString();
+            }
+    }
+    return "";
+}
 static string EvaluateBrackets(string expression)
 {
+    string[] allowedFuncs = new string[] { "log", "ln", "exp", "sqrt", "abs", "atan", "acos", "asin", "sinh", "cosh", "tanh", "tan", "sin", "cos" };
+    var functionsInExpression = allowedFuncs.Where(func => expression.Contains(func));
     Regex oper = new(@"\+|\*|\/|&|-");
     var operators = oper.Matches(expression);
+    if (functionsInExpression.Any()) 
+    {
+        foreach (var func in functionsInExpression)
+        {
+            var startOfFunc = expression.IndexOf(func);
+            string function = expression.Substring(startOfFunc, func.Length);
+            string value = "";
+            if (operators.Count == 3)
+            {
+                var endOfFunc = expression.IndexOf(function[^1]) + 1;
+                var oneBeforeOperator = endOfFunc < operators[1].Index ? operators[1].Index - 1 : expression.Length;
+                value = expression.Substring(endOfFunc, oneBeforeOperator - endOfFunc == 0 ? 1 : oneBeforeOperator - endOfFunc);
+            } else if (operators.Count == 2) 
+            {
+                var notDashOperator = operators.Where(oper => oper.Value != "-").First();
+                var endOfFunc = expression.IndexOf(function[^1]) + 1;
+                var oneBeforeOperator = endOfFunc < notDashOperator.Index ? notDashOperator.Index - 1 : expression.Length;
+                value = expression.Substring(endOfFunc, oneBeforeOperator - endOfFunc == 0 ? 1 : oneBeforeOperator - endOfFunc);
+            } else
+            {
+                var endOfFunc = expression.IndexOf(function[^1]) + 1;
+                var oneBeforeOperator = startOfFunc < operators.First().Index ? operators.First().Index - 1 : expression.Length;
+                value = expression.Substring(endOfFunc, oneBeforeOperator - endOfFunc == 0 ? 1 : oneBeforeOperator - endOfFunc);
+            }
+            expression = expression.Replace(function + value, EvaluateFunction(function, value));
+        }
+    }
     string[]? numbers = new string[2];
+    operators = oper.Matches(expression);
+
     if (operators.Count == 3)
     {
         numbers = expression.Split(operators[1].Value);
@@ -132,7 +178,7 @@ static double Multiply(double left, double right) => left * right;
 static double Divide(double left, double right) => left / right;
 static double Add(double left, double right) => left + right;
 
-Eval("sqrt (s in(2 + 3)*c os (1+2)) * 4 & 2)");
+Eval("sqrt (sin(2 + 3)*cos (1-2)) * 4 & 2");
 Eval("  ( ---(   2 + 3)    * (123 - 2222)) * 4 & 2");
 
 
